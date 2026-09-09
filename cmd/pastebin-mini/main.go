b845b0e649557337439dbd1c9074aa624a4f19c2
Dmitry <born@to.win>
2026-09-09 20:44:44 +0300

    setup logger

--- cmd/pastebin-mini/main.go
+++ cmd/pastebin-mini/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
 	"github.com/username/pastebin-mini/internal/config"
 )
 
 func main() {
 	cfg := config.MustLoad()
+
+	logger := setupLogger(cfg.Env)
+
+	logger.Debug("test")
+}
+
+func setupLogger(env string) *slog.Logger {
+	switch env {
+	case "local":
+		return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "dev":
+		return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case "prod":
+		return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	default:
+		panic("unknown environment: " + env)
+	}
 }
