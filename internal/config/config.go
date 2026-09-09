dea3166189c1b360b32d0aa44c2aebf8518d80bc
Dmitry <born@to.win>
2026-09-09 20:20:03 +0300

    add config loading

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string           `yaml:"env"`
+	StoragePath string           `yaml:"storage_path"`
+	HttpServer  HttpServerConfig `yaml:"http_server"`
+}
+
+type HttpServerConfig struct {
+	Address     string        `yaml:"address"`
+	Timeout     time.Duration `yaml:"timeout"`
+	TimeoutIdle time.Duration `yaml:"timeout_idle"`
+}
+
+func mustLoad() *Config {
+	cfgPath := os.Getenv("CONFIG_PATH")
+	if cfgPath == "" {
+		log.Fatal("config path not exists")
+	}
+
+	if _, err := os.Stat(cfgPath); os.IsNotExist(err) {
+		log.Fatalf("config not exists, %s", cfgPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(cfgPath, &cfg); err != nil {
+		log.Fatalf("cannot read config: %v", err)
+	}
+
+	return &cfg
+}
