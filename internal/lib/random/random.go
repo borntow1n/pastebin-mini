0420d18aeacc85de46cf7620255f117f1f9d0380
Dmitry <born@to.win>
2026-09-10 16:39:55 +0300

    create random lib

---
+++ internal/lib/random/random.go
@@ -0,0 +1,16 @@
+package random
+
+import (
+	"math/rand/v2"
+)
+
+func NewRandomString(lenght int) string {
+	letters := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	alias := make([]byte, lenght)
+	for n := range alias {
+		alias[n] = letters[rand.IntN(len(letters))]
+	}
+
+	return string(alias)
+}
